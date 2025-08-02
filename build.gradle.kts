46cf9deb584ba2324d766cda0d8fc5783d3a2b3f
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2025-08-03 06:47:24 +0800

    fix: gradle config cache issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,55 @@
 plugins {
-	alias(libs.plugins.kotlin)
-	alias(libs.plugins.spotless)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.spotless)
 }
 
 group = "io.arthurkun"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	testImplementation(kotlin("test"))
+    testImplementation(kotlin("test"))
 }
 tasks.test {
-	useJUnitPlatform()
-	testLogging {
-		showStandardStreams = true
-		events("passed", "failed", "skipped")
-	}
+    useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+        events("passed", "failed", "skipped")
+    }
 }
 kotlin {
-	jvmToolchain(17)
+    jvmToolchain(17)
 }
 
 spotless {
-	ratchetFrom("origin/master")
-	kotlin {
-		target("**/*.kt", "**/*.kts")
-		targetExclude("**/build/**/*.kt")
-		ktlint(libs.ktlint.core.get().version).editorConfigOverride(
-			mapOf("ktlint_standard_annotation" to "disabled"),
-		)
-		trimTrailingWhitespace()
-		endWithNewline()
-	}
-	format("xml") {
-		target("**/*.xml")
-		trimTrailingWhitespace()
-		endWithNewline()
-	}
+    ratchetFrom("origin/master")
+    kotlin {
+        target("**/*.kt", "**/*.kts")
+        targetExclude("**/build/**/*.kt")
+        ktlint(libs.ktlint.core.get().version).editorConfigOverride(
+            mapOf("ktlint_standard_annotation" to "disabled"),
+        )
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    format("xml") {
+        target("**/*.xml")
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 tasks.register<Copy>("installLocalGitHook") {
-	from(File("${project.rootDir}/pre-push"))
-	into(File(rootProject.rootDir, ".git/hooks"))
-	doLast {
-		val hookFile = File(rootProject.rootDir, ".git/hooks/pre-push")
-		hookFile.setExecutable(true, false)
-	}
+    from(layout.projectDirectory.file("pre-push"))
+    into(layout.projectDirectory.dir(".git/hooks"))
+    doLast {
+        val hookFile = layout.projectDirectory.file(".git/hooks/pre-push").asFile
+        hookFile.setExecutable(true, false)
+    }
 }
 tasks.named("build") {
-	dependsOn("installLocalGitHook")
+    dependsOn("installLocalGitHook")
 }
