852f92cc231ee1a15b37328281c6a2ef85c81c64
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2025-08-02 10:03:50 +0800

    test: enhance assertions in MultiTargetTest for clarity and accuracy

--- src/test/kotlin/commands/MultiTargetTest.kt
+++ src/test/kotlin/commands/MultiTargetTest.kt
@@ -2,69 +2,48 @@ import io.arthurkun.parser.model.AutoSkillAction
 import io.arthurkun.parser.model.AutoSkillCommand
 import io.arthurkun.parser.model.SkillActionsTarget
 import io.arthurkun.parser.model.SkillSource
-import kotlin.test.Test
-import kotlin.test.assertFails
+import kotlin.test.*
 
 class MultiTargetTest {
-    companion object Companion {
-        private const val COMMAND = "b([Ch2B]2)4"
-    }
-
-    val parsedCommand: AutoSkillCommand = AutoSkillCommand.parse(COMMAND)
-
-    @Test
-    fun `commands should not be empty`() {
-        assert(parsedCommand.stages.isNotEmpty()) {
-            "Parsed command should not be empty"
-        }
-    }
 
     @Test
     fun `Kukulcan 2nd skill Multi-target`() {
+        val command = "b([Ch2B]2)"
+        val parsedCommand: AutoSkillCommand = AutoSkillCommand.parse(command)
         val stage1turn1 = parsedCommand[0, 0]
 
-        assert(stage1turn1.isNotEmpty()) {
-            "First stage and turn should have actions"
+        assertTrue {
+            stage1turn1.isNotEmpty()
         }
 
         val action1 = stage1turn1[0]
-        assert(action1.codes == "b([Ch2B]2)") {
-            "First action should have correct codes"
-        }
+        assertIs<AutoSkillAction.ServantSkill>(action1)
 
-        assert(
-            action1 is AutoSkillAction.ServantSkill &&
-                    action1.skillSource is SkillSource.Servant.AS2
-        ) {
-            "First action should be AS2 skill"
-        }
+        assertEquals(
+            expected = command,
+            actual = action1.codes,
+            message = "Action codes should be '$command'"
+        )
 
-        assert(
-            action1 is AutoSkillAction.ServantSkill &&
-                    action1.targets.size == 2
-        ) {
-            "First action should have 2 targets"
-        }
+        assertEquals(
+            expected = 2,
+            actual = action1.targets.size,
+            message = "Action should have two targets"
+        )
+        assertIs<SkillSource.Servant.AS2>(action1.skillSource)
 
-        assert(
-            action1 is AutoSkillAction.ServantSkill &&
-                    action1.targets[0] is SkillActionsTarget.SpecialSkill.Choice2OptionB
-        ) {
-            "First target should be Choice2OptionB"
-        }
+        assertIs<SkillActionsTarget.SpecialSkill.Choice2OptionB>(
+            action1.targets[0],
+            message = "Action target should be Choice2OptionB"
+        )
 
-        assert(
-            action1 is AutoSkillAction.ServantSkill &&
-                    action1.targets[1] is SkillActionsTarget.B
-        ) {
-            "Second target should be B"
-        }
+        assertIs<SkillActionsTarget.B>(
+            action1.targets[1],
+            message = "Action target should be B"
+        )
 
-        assertFails(
-            "There's no 3rd target in this action",
-        ) {
-            val currentAction = action1 as AutoSkillAction.ServantSkill
-            currentAction.targets[2]
+        assertFails("There's no 3rd target in this action") {
+            action1.targets[2]
         }
     }
 }
